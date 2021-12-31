3443bd05b4cbb7e5d5ed0a62115ee8d1c49d3d9b
marffm <marceloffmartins@hotmail.com>
2021-12-31 17:04:35 +0100

    Add context test to Posts api

---
+++ posts-api/src/test/java/com/marffm/postsapi/PostsApiApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.marffm.postsapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostsApiApplicationTest {
+
+    @Test
+    fun contextLoad() {
+    }
+
+}
